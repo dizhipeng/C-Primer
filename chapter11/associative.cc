b3cbc7a28fa4937f34972832a8f36d99f8aaeee5
Dirischlecht <dirischlecht@outlook.com>
2019-03-24 17:10:11 +0800

    map insertion, multimap/multiset test, and other miscellaneous tests.

--- chapter11/associative.cc
+++ chapter11/associative.cc
@@ -14,6 +14,7 @@ using std::endl;
 using std::string;
 using std::map;
 using std::set;
+using std::multimap;
 using std::multiset;
 using std::ifstream;
 using std::vector;
@@ -61,9 +62,26 @@ int main(int argc,char **argv)
         //only count words not in the blacklist
         if(not_in_black_list(word,bl))
         {
+#ifndef INSERT_MAP
             //increment the count
             //when key does'nt exist, value init to 0 automatically
             ++word_count[word];
+#else
+            //appeard once, try to insert as if the element doesn't exist
+            //return type of insert() is a pair
+            pair<map<string,size_t>::iterator,bool> ins_pair=word_count.insert({word,1});
+
+            //if already exists, the insertion will fail
+            //just increament the existing element
+            if(!ins_pair.second)
+            {
+                ++ins_pair.first->second;
+            }
+
+            //another way of the same operation 
+            ++word_count.insert({word,0}).first->second;
+            --word_count.insert({word,0}).first->second;
+#endif
         }
     }
 
@@ -73,6 +91,7 @@ int main(int argc,char **argv)
         cout<<ele.first<<" appears "<<ele.second<<" time(s) "<<endl;
     }
 
+#ifndef MULTI_MAP
     map<string,vector<string>> families=
     {{"bovary",{"charles","emma"}},{"meursault",{"mary","raymon"}}};
 
@@ -82,6 +101,21 @@ int main(int argc,char **argv)
 
     //change values
     families["meursault"].push_back("masson");
+#else
+    multimap<string,vector<string>> families=
+    {{"bovary",{"charles","emma"}},{"meursault",{"mary","raymon"}}};
+
+    //add new pairs
+    families.insert({"exuperee",{"prince","roi","serpent"}});
+
+    //change values
+    auto mulm_it=families.find("meursault"); 
+    if(mulm_it!=families.end())
+    {
+        mulm_it->second.push_back("masson");
+    }
+#endif
+
 
     vector<int> vec_int={3,4,5,5,4,3,2,1,1,3,2};
 
@@ -121,9 +155,9 @@ int main(int argc,char **argv)
     //so it can't be keys
     //map<list<int>::iterator,int> list_map={{ls.begin(),*ls.begin()}};
 
-    vector<int> vs={3,4};
+    vector<int> vi={3,4};
     //vector iterator is allowed to be keys
-    map<vector<int>::iterator,int> vector_map={{vs.begin(),*vs.begin()}};
+    map<vector<int>::iterator,int> vector_map={{vi.begin(),*vi.begin()}};
 
     ifs1.close();
     ifstream ifs2("words");
@@ -166,6 +200,10 @@ int main(int argc,char **argv)
     //function pointer type in <>
     //function pointer as initializer in ()
     multiset<Sales_data,comp> books(compareIsbn);
+    
+    //get the iterator of this multiset
+    multiset<Sales_data,comp>::iterator muls_it=books.begin();
+    (void)muls_it;
 
     ifstream ifs3("pairs");
     if(!ifs3)
@@ -225,6 +263,58 @@ int main(int argc,char **argv)
         }
         cout<<"\b\b ";
     }
+
+    auto word_count2=word_count;
+    for(auto beg=word_count2.begin();beg!=word_count2.end();++beg)
+    {
+        //for maps key_type is const, assignment is not allowed
+        //*beg={"test",1};
+        //beg->first="test";
+        beg->second=100;
+    }
+
+    vector<string> vs={"c","b","a"};
+    multiset<string> ss={"c","b","a"};
+
+    copy(vs.begin(),vs.end(),inserter(ss,ss.end()));
+    //multiset has not push_back() member
+    //copy(vs.begin(),vs.end(),back_inserter(ss));
+    cout<<endl<<"Elements inserted in set: ";
+    for(const auto &ele:ss)
+    {
+        cout<<ele<<" ";
+    }
+
+    //calling vs.insert(vs.end(),val) and vs.push_back()
+    //to insert elements in vector vs
+    copy(ss.begin(),ss.end(),inserter(vs,vs.end()));
+    copy(ss.begin(),ss.end(),back_inserter(vs));
+    cout<<endl<<"Elements inserted in vector: ";
+    for(const auto &ele:vs)
+    {
+        cout<<ele<<" ";
+    }
+
+    cout<<endl<<"access elements using iterator of a map: "<<endl;
+    map<string,size_t>::const_iterator map_it=word_count.cbegin();
+    while(map_it!=word_count.cend())
+    {
+        cout<<map_it->first<<" appears "<<map_it->second<<" time(s) "<<endl;
+        ++map_it;
+    }
+
+    map<string,vector<int>> s_to_vi;
+    //type of the insert() argument, namely the value_type
+    pair<string,vector<int>> elem={"insert type test: ",{1,2,3}};
+    //type of the return value of insert(), a pair of {iterator,bool}
+    pair<map<string,vector<int>>::iterator,bool> ret=s_to_vi.insert(elem);
+    //print the element
+    cout<<ret.first->first;
+    for(const auto &ele:ret.first->second)
+    {
+        cout<<ele<<" ";
+    }
+    cout<<endl;
 
     return EXIT_SUCCESS;
 
